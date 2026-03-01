dbd2e3274c913754ae1ee369995613c953de1142
BeansReal1 <ignavent@gmail.com>
2026-03-01 16:30:38 -0300

    we got cooldowns

--- example_mods/heyMod/data/liquid/mechanics.lua
+++ example_mods/heyMod/data/liquid/mechanics.lua
@@ -1,4 +1,12 @@
 function onCreate()
+	isParrying = false
+	isParryingActive = false
+	canParry = true
+
+	activeParryTime = 0.5
+	cooldownParryTime = 0.2
+	currentParryTime = 0
+
 
 	makeAnimatedLuaSprite('parryEffect', 'roa/parryEffect', 'boyfriend.x', 'boyfriend.y');
 	addAnimationByPrefix('parryEffect', 'parry', 'parry effect', 24, false);
@@ -7,35 +15,35 @@ function onCreate()
 	addHaxeLibrary('ColorSwap', 'shaders')
 end
 
-function onUpdate()
+function onUpdate(dt)
 
-	if keyboardJustPressed('SPACE') then
-		setProperty("parryEffect.alpha", 1) 
-		playAnim("parryEffect", "parry", true)
-		setProperty("boyfriend.specialAnim", true) 
-
-		
-		runHaxeCode([[
-        	var colorSwap = new ColorSwap();
-        	game.boyfriend.shader = colorSwap.shader;
-        	colorSwap.hue = 0/360;
-        	colorSwap.saturation = 0/100;
-        	colorSwap.brightness = 80/100;
-		]])
+	if keyboardJustPressed('SPACE') and canParry then
+		startParry()
 	end
 
 	if getProperty('parryEffect.animation.curAnim.finished') and getProperty('parryEffect.animation.curAnim.name') == 'parry' then
-		setProperty("parryEffect.alpha", 0);
+		endParry()
+	end
 
-		runHaxeCode([[
-        	var colorSwap = new ColorSwap();
-        	game.boyfriend.shader = colorSwap.shader;
-        	colorSwap.hue = 0/360;
-        	colorSwap.saturation = 0/100;
-        	colorSwap.brightness = 0/100;
-		]])
+	if isParrying then 
+		currentParryTime = currentParryTime + dt
+		
+		if currentParryTime <= activeParryTime then 
+			isParryingActive = true
+			canParry = false
+		elseif currentParryTime <= activeParryTime + cooldownParryTime then
+			isParryingActive = false 
+			canParry = false 
+		else 
+			isParryingActive = false 
+			canParry = true
+			isParrying = false
+			currentParryTime = 0
+		end
 
 	end
+
+
 
 end
 
@@ -43,4 +51,32 @@ function opponentNoteHit(b, t, s)
 	if getHealth()>0.1 then
 		setHealth(getHealth()- 0.01)
 	end
+end
+
+function startParry()
+	isParrying = true
+	setProperty("parryEffect.alpha", 1) 
+	playAnim("parryEffect", "parry", true)
+	setProperty("boyfriend.specialAnim", true) 
+
+		
+	runHaxeCode([[
+        var colorSwap = new ColorSwap();
+        game.boyfriend.shader = colorSwap.shader;
+        colorSwap.hue = 0/360;
+        colorSwap.saturation = 0/100;
+        colorSwap.brightness = 80/100;
+	]])
+end 
+
+function endParry()
+	setProperty("parryEffect.alpha", 0);
+
+	runHaxeCode([[
+        var colorSwap = new ColorSwap();
+        game.boyfriend.shader = colorSwap.shader;
+        colorSwap.hue = 0/360;
+        colorSwap.saturation = 0/100;
+        colorSwap.brightness = 0/100;
+	]])
 end
