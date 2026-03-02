d5954f35de5e3dcba79cfbc2bc75671945299ab1
heyimyabo <gusierivera@gmail.com>
2026-03-02 07:01:29 -0500

    placeholder starlight stage

---
+++ example_mods/heyMod/stages/starlightStage.lua
@@ -0,0 +1,20 @@
+function onCreate()
+	-- background shit
+	makeLuaSprite('bg','',-3000,-1500)
+	makeGraphic('bg',8500,4000,'ffffff')
+	addLuaSprite('bg',false)
+	
+	makeLuaSprite('speakerLeft', 'roa/speakerSketch', -200, -20);
+	setLuaSpriteScrollFactor('speakerLeft', 1.02, 1);
+	addLuaSprite('speakerLeft', false);
+
+	makeLuaSprite('speakeRight', 'roa/speakerSketch', 1650, -20);
+	setLuaSpriteScrollFactor('speakeRight', 1.02, 1);
+	addLuaSprite('speakeRight', false);
+	setProperty('speakeRight.scale.x', -1)
+
+	makeLuaSprite('stage', 'roa/stageSketch', -410, 835);
+	addLuaSprite('stage', false);
+
+
+end
