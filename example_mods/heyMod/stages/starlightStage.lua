8b587f68916848ed9458992f994f6dcd5c8e50ef
heyimyabo <gusierivera@gmail.com>
2026-03-07 20:14:02 -0500

    note swap medias

--- example_mods/heyMod/stages/starlightStage.lua
+++ example_mods/heyMod/stages/starlightStage.lua
@@ -87,3 +87,27 @@ function onTweenCompleted(tag)
 		doTweenX('noteLoopTween2', 'noteLoop2', getProperty('noteLoop2.x') + width, noteTweenTime, 'linear')
 	end
 end
+
+function onUpdate()
+
+	--note swap medias
+	if curBeat == 0 then
+		setPropertyFromGroup('playerStrums', 0, 'x', defaultOpponentStrumX0)
+
+		setPropertyFromGroup('playerStrums', 1, 'x', defaultOpponentStrumX1)
+
+		setPropertyFromGroup('playerStrums', 2, 'x', defaultOpponentStrumX2)
+
+		setPropertyFromGroup('playerStrums', 3, 'x', defaultOpponentStrumX3)
+
+
+        setPropertyFromGroup('opponentStrums', 0, 'x', defaultPlayerStrumX0 + 0)
+
+        setPropertyFromGroup('opponentStrums', 1, 'x', defaultPlayerStrumX1 + 0)
+
+        setPropertyFromGroup('opponentStrums', 2, 'x', defaultPlayerStrumX2 + 0)
+
+        setPropertyFromGroup('opponentStrums', 3, 'x', defaultPlayerStrumX3 + 0)
+
+	end
+end
